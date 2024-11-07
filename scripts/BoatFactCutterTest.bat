dfc11dd82821f5d4098829f426902c43e6ada246
jcanell4 <jcanell4@xtec.cat>
2024-11-07 13:30:17 +0100

    Version 1.1-SNAPSHOT finished. Changing scripts

---
+++ scripts/BoatFactCutterTest.bat
@@ -0,0 +1,4 @@
+@echo off
+
+java -jar boat_fact_extractor.jar cut_test %1 %2 %3 %4 %5 %6 %7 %8 %9 %10 %11 %12 %13 %14 %15 %16
+
--- scripts/BoatFactExtractor.bat
+++ scripts/BoatFactExtractor.bat
@@ -1,3 +1,5 @@
-cmd /k "java -jar boat_fact_extractor.jar"
+@echo off
+
+java -jar boat_fact_extractor.jar  %1 %2 %3 %4 %5 %6 %7 %8 %9 %10 %11 %12 %13 %14 %15 %16 %17
 
 
